02f817fd29c394c99eec8adeb2933786ba91f32d
SinRedempti0n <Xsatarix@gmail.com>
2021-02-08 21:55:24 +0000

    first commit

---
+++ src/com/sinredemption/Main.java
@@ -0,0 +1,11 @@
+package com.sinredemption;
+
+public class Main{
+
+    public static void main(String args[]) {
+        Deadlock.Thread1 T1 = new Deadlock.Thread1();
+        Deadlock.Thread2 T2 = new Deadlock.Thread2();
+        T1.start();
+        T2.start();
+     }
+}
